17013e46af355e7c7055d211ebef72a9adc0b3e5
Henry Adusei <henryadusei@gmail.com>
2020-03-06 15:58:46 -0500

    retrieved props to pass to the suggeested card component

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -75,6 +75,8 @@ class NewTrip extends Component {
     }
 
     render() {
+        console.log(this.state.cityId);
+        
         return(
             <div className="NewTrip">
                 <RestaurantList cityId={this.state.cityId}/>

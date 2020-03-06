41489c9e81028f9185d78ad5782eb03f926389f2
Hassanselsayed <hsaadelsayed@gmail.com>
2020-03-06 11:01:36 -0500

    suggest North American cities on typing + get selected city ID

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import RestaurantList from './RestaurantList'
+import RestaurantList from './RestaurantList';
+import Suggestions from './Suggestions';
 
 // user types city name in input field,
 // axios call is made to retrieve an array of suggested cities that match
@@ -17,45 +18,85 @@ class NewTrip extends Component {
         super();
         this.state = {
             trip: {},
-            cityID: '',
-            suggestedCities: [],
+            cityName: "",
+            cityId: '',
+            suggestedCities: []
         }
     }
 
-    componentDidMount() {
+    getInfo = (cityName) => {
         axios({
-            url: `https://developers.zomato.com/api/v2.1/cities`,
+            url: `https://developers.zomato.com/api/v2.1/cities?q=${cityName}`,
             method: "GET",
             responseType: "json",
             headers: {
-                "user-key": "cff8655f9125581c7db4a5e95cd60d6f",
-            },
-            params: {
-                q: 'toronto'
+                "user-key": "7cec49712d95851f70de3b8beaf245d9",
             }
         }).then((res) => {
+            const northAmericanCities = res.data.location_suggestions.filter((value, index) => {
+                if (value.country_id === 37 || value.country_id === 216 ) {
+                    return value
+                }
+            });
+            const topSuggestions = northAmericanCities.filter((value, index) => {
+                return index <=4
+            })
                 this.setState({
-                    suggestedCities: res.data.location_suggestions
+                    suggestedCities: topSuggestions 
                 })
         }).catch((error) => {
             console.log(error)
         })
     }
 
+    getCityId = (e) => {
+        e.preventDefault();
+        const copyOfSuggestedCities = [...this.state.suggestedCities];
+        const suggestedCitiesNames = copyOfSuggestedCities.filter((city) => {
+            return this.state.cityName === city.name;
+        })
+        this.setState({
+            cityId: suggestedCitiesNames[0].id
+        })
+
+    }
+
+
+    handleInputChange = () => {
+        this.setState({
+            cityName: this.search.value
+        }, () => {
+                if (this.state.cityName) {
+                    if (this.state.cityName.length % 2 === 0) {
+                        this.getInfo(this.state.cityName)
+                }
+                } else {
+
+                }
+        })
+    }
+
     render() {
         return(
-            <div>
-                <RestaurantList />
+            <div className="NewTrip">
+                <RestaurantList cityId={this.state.cityId}/>
                 <section>
-                    <form action="">
+                    <form action="" onSubmit={this.getCityId}>
                         <h3>new trip</h3>
                         <label htmlFor="tripName">Please enter a name for your trip</label>
                         <input type="text" id="tripName" />
                         <label htmlFor="citySearch">Where are you going?</label>
-                        <input type="search" id="citySearch" />
+                        <input
+                        autoComplete="off"
+                            type="search"
+                            id="citySearch"
+                            ref={input => this.search = input}
+                            onChange={this.handleInputChange}
+                        />
                         <button>save trip</button>
                     </form>
                 </section>
+                <Suggestions results={this.state.suggestedCities} />
             </div>
         )
     }

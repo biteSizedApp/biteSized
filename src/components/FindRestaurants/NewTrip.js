852bfbf6f1daaf65584ad59761c66d7ce24b4369
Olga <olga.filiushkina@gmail.com>
2020-03-09 15:20:38 -0400

    fixed imports

--- src/components/FindRestaurants/NewTrip.js
+++ src/components/FindRestaurants/NewTrip.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 import SuggestedRestaurantList from './SuggestedRestaurantList';
 import Suggestions from './Suggestions';
-// import Autosuggest from 'react-autosuggest';
 
 // user types city name in input field,
 // axios call is made to retrieve an array of suggested cities that match

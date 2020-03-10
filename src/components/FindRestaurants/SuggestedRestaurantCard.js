de397ac840acca7f8ec466848083a235b63ec7d4
Olga <olga.filiushkina@gmail.com>
2020-03-09 20:20:42 -0400

    fixed a wrong merge in suggestedRestaurantComponent

--- src/components/FindRestaurants/SuggestedRestaurantCard.js
+++ src/components/FindRestaurants/SuggestedRestaurantCard.js
@@ -25,25 +25,8 @@ class SuggestedRestaurantCard extends Component {
         }
     }
 
-    addToList = (e) => {
-        this.props.addRestaurantToList(e, this.state.restaurant);
-        this.setState ({
-            added: true,
-        })
-    }
 
     render() {
-        let elementToDisplay;
-
-        if (!this.state.added) {
-            return (
-                <button onClick={this.addToList}><i className="fas fa-plus" aria-hidden></i>Add to list</button>
-            )
-        } else {
-            return (
-                <p><i class="fas fa-check" aria-label="added to restaurant list"></i></p>
-            )
-        }
 
         return (
             <div className="SuggestedRestaurantCard">

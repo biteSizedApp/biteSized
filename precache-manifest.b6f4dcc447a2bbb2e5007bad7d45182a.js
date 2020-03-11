d96c2887df320db6c73c6586587f22f8731d2f76
Olga <olga.filiushkina@gmail.com>
2020-03-11 15:34:01 -0400

    Updates

---
+++ precache-manifest.b6f4dcc447a2bbb2e5007bad7d45182a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdca3a700fbf67f179c1628e289ba288",
+    "url": "/biteSized/index.html"
+  },
+  {
+    "revision": "c5623056956421e8a13d",
+    "url": "/biteSized/static/css/main.1241fd1d.chunk.css"
+  },
+  {
+    "revision": "b3d5aeb304e6680e240b",
+    "url": "/biteSized/static/js/2.e7320b92.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/biteSized/static/js/2.e7320b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5623056956421e8a13d",
+    "url": "/biteSized/static/js/main.81e36b66.chunk.js"
+  },
+  {
+    "revision": "ee32854a37a5fcbbaed5",
+    "url": "/biteSized/static/js/runtime-main.116d607a.js"
+  }
+]);

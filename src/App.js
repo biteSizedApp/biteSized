086b97ee33137938dba8211afbf628ff51639fdf
Henry Adusei <henryadusei@gmail.com>
2020-03-11 12:53:46 -0400

    fixed paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import NewTrip from './components/FindRestaurants/NewTrip';
-import YourTrips from './components/YourTrip/YourTrips';
+import NewTrip from './components/findRestaurants/NewTrip';
+import YourTrips from './components/yourTrip/YourTrips';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
